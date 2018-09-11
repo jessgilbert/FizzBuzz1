723dba0acf4460c4a4a9928080477f4d2fec4b0e
Jess <jessica.gilbert@softwire.com>
2018-09-11 10:35:59 +0100

    With extra method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,36 +5,50 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        for (Integer i = 0; i <= 100; i++) {
+        for (Integer i = 0; i <= 100; i = i + 1) {
 
-            if (i % 15 == 0) {
+            String x = calculateFizzBuzzForNumber(i);
+
+            if (i % 11 == 0) {
+                System.out.println("Bong");
+            }
+            else if (i % 15 == 0) {
                 //If number is factor of 3 and 5
                 System.out.println("FizzBuzz");
                 //Print 'FizzBuzz'
-            } else if (i % 35 == 0) {
+            }
+            else if (i % 13 == 0) {
+                System.out.println("Fezz");
+            }
+            else if (i % 35 == 0) {
                 //If number factor of 7 and 5
                 System.out.println("BuzzBang");
                 //Print BuzzBang
 
-            } else if (i % 21 == 0) {
+            }
+            else if (i % 21 == 0) {
                 //If number factor of 3 and 7
                 System.out.println("FizzBang");
                 //Print 'FizzBang'
-            } else if (i % 3 == 0) {
+            }
+            else if (i % 3 == 0) {
                 //If number is factor of 3
                 System.out.println("Fizz");
                 //Print 'Fizz'
-            } else if (i % 5 == 0) {
+            }
+            else if (i % 5 == 0) {
                 //If number is factor of 5
                 System.out.println("Buzz");
                 //Print 'Buzz'
-            } else if (i % 7 ==0) {
+            }
+            else if (i % 7 == 0) {
                 //If number is factor of 7
                 System.out.println("Bang");
                 //Print 'Bang'
 
 
-            } else {
+            }
+            else {
                 System.out.println(String.valueOf(i));
                 //Print i (Number 1 to 100) using numbers if not otherwise specified
             }
@@ -42,4 +56,23 @@ public class Main {
         }
 
     }
+
+    private static String calculateFizzBuzzForNumber(Integer i) {
+        String result = "";
+
+        if (i % 3 == 0) {
+            result = result + "Fizz";
+        }
+        if (i % 5 == 0) {
+            result = result + "Buzz";
+        }
+        if (i % 7 == 0) {
+            result = result + "Bang";
+        }
+
+
+        return result;
+    }
+
+
 }
